e0883fc2e25ec98a7f4551f4fef9a10c52cb26ff
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-12-29 14:14:38 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,8 +1,8 @@
 if [ -x "$(command -v aria2c)" ] ; then
         pkg install update
 	cd ~
-	screen -dmS aria2 aria2c --enable-rpc --rpc-listen-all
-	screen -dmS aria3 aria3c --enable-rpc --rpc-listen-all --conf-path=/data/data/com.termux/files/home/.config/aria2/aria3.conf
+	screen -dmS aria2 aria2c --enable-rpc --conf-path=/data/data/com.termux/files/home/.config/aria2/aria2.conf &
+	screen -dmS aria3 aria3c --enable-rpc --conf-path=/data/data/com.termux/files/home/.config/aria2/aria3.conf &
 	screen -ls
 	echo "aria2双开已完成，screen -r aria2即可查看，screen -r aria3查看另一个。"
 else
@@ -17,9 +17,9 @@ else
 	echo "rpc-listen-port=6800" > aria2.conf
 	echo "rpc-listen-port=6801" > aria3.conf
 	cd ~
-	screen -dmS aria2 aria2c --enable-rpc --rpc-listen-al &
-	screen -dmS aria3 aria3c --enable-rpc --rpc-listen-all --conf-path=/data/data/com.termux/files/home/.config/aria2/aria3.conf &
-	screen -ls
+	screen -dmS aria2 aria2c --enable-rpc --conf-path=/data/data/com.termux/files/home/.config/aria2/aria2.conf &
+	screen -dmS aria3 aria3c --enable-rpc --conf-path=/data/data/com.termux/files/home/.config/aria2/aria3.conf &
+screen -ls
 	echo "aria2双开已完成，screen -r aria2即可查看，screen -r aria3查看另一个。"
 fi
 exit
